b33682e6d095f1c65cc077f8db435132cc3407d9
Paiano, Chris (ETW) <Chris.Paiano@nike.com>
2014-06-01 10:49:29 -0700

    -Set up simple media player and used test stream URL from the BBC for proof of concept.  Must kill the app in order to shut up the stream, but hey its a start :)

--- src/com/deadcircuit/deadcircuitradio/RadioActivity.java
+++ src/com/deadcircuit/deadcircuitradio/RadioActivity.java
@@ -1,7 +1,13 @@
 package com.deadcircuit.deadcircuitradio;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.io.IOException;
 
 public class RadioActivity extends Activity
 {
@@ -11,5 +17,27 @@ public class RadioActivity extends Activity
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+
+    //Initialize the uri view with a shoutcast audio stream so that we can have mediacontroller controls for very little code
+/*
+    VideoView videoView = (VideoView) findViewById(R.id.video_view);
+    MediaController mediaController = new MediaController(this);
+    mediaController.setAnchorView(videoView);
+*/
+// Set uri link (mp4 format )
+    Uri uri = Uri.parse(getString(R.string.shoutcast_stream_url));
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    try {
+      mediaPlayer.setDataSource(this, uri);
+      mediaPlayer.prepare();
+      mediaPlayer.start();
+    } catch (IOException e) {
+      e.printStackTrace(); //To change body of catch statement use File | Settings | File Templates.
+    }
+/*
+    videoView.setMediaController(mediaController);
+    videoView.setVideoURI(uri);
+    videoView.start();
+*/
   }
 }
